1a5a1c667693645641955cd0813b26e3167d3806
dukewellington61 <dukewellington61@googlemail.com>
2020-06-11 23:46:48 +0200

    redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,26 @@
 import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
+
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
 
 import "./App.css";
 
 const App = () => (
-  <Fragment>
-    <Navbar />
-    <Landing />
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Provider store={store}>
+        <Fragment>
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <Landing />
+        </Fragment>
+      </Provider>
+    </Fragment>
+  </Router>
 );
 
 export default App;
