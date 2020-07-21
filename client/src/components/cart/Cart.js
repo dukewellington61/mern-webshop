099113df0a93d1169b8995d31b12391822bfab2e
akoeth-portfolio <dukewellington61@googlemail.com>
2020-07-21 09:46:20 +0200

    2107 cart pull in all products no good

--- client/src/components/cart/Cart.js
+++ client/src/components/cart/Cart.js
@@ -1,28 +1,36 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { Fragment, useEffect } from "react";
+// import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
-const Cart = ({ line_items, products }) => {
+import { getProducts } from "../../actions/product";
+
+const Cart = ({ line_items, products, getProducts }) => {
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   const productIds = line_items.map((line_item) => line_item.product_id);
 
   const productsInCart = products.filter(
-    (product) => productIds.indexOf(product._id) === -1
+    (product) => productIds.indexOf(product._id) !== -1
   );
 
   return (
-    <div>
-      {productsInCart.map((productInCart) => (
-        <div>{productInCart.name}</div>
-      ))}
-    </div>
+    <Fragment>
+      <div style={{ position: "absolute", top: "500px", color: "white" }}>
+        {productsInCart.map((productInCart) => (
+          <div key={productInCart._id}>{productInCart.name}</div>
+        ))}
+      </div>
+    </Fragment>
   );
 };
 
-Cart.propTypes = {};
+// Cart.propTypes = {};
 
 const mapStateToProps = (state) => ({
   line_items: state.cart.cart.line_items,
   products: state.product.products,
 });
 
-export default connect(mapStateToProps, null)(Cart);
+export default connect(mapStateToProps, { getProducts })(Cart);
