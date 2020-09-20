9cd327d05a2f9444b3500c34456cc0f699150566
akoeth-portfolio <dukewellington61@googlemail.com>
2020-09-21 00:27:15 +0200

    render order solved

--- client/src/components/order/OrderHeaders.js
+++ client/src/components/order/OrderHeaders.js
@@ -1,16 +1,20 @@
-import React, { Fragment, useState, useEffect } from "react";
+import React, { Fragment, useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 import { calculateGrandTotal } from "../../utils/calcGrandTotal";
 import Order from "./Order";
 
 const OrderHeaders = ({ order }) => {
+  // stop the order component from beeing initially rendered
   useEffect(() => {
     document.getElementById(order._id).classList.add("not_render_orders");
   }, []);
 
-  const [angleUp, setAngleUp] = useState(true);
+  // is the font awsome icon element in the current instance of @components/order/OrderHeaders.js (the one that has been clicked)
+  const angleElement = useRef(null);
 
   const handleClick = (id) => {
+    // stops the previous order (the one that has been displayed before clicking the current order) from beeing displayed
+    // if previous order and current order are the same i.e. user clicks on same OrderHeaders element twice nothing happens
     document
       .querySelectorAll(".orders")
       .forEach((orderElement) =>
@@ -19,20 +23,31 @@ const OrderHeaders = ({ order }) => {
           : null
       );
 
+    // sets the previous instance of @components/order/OrderHeaders.js to up (which is in line with what happens above)
     document
       .querySelectorAll(".fa-angle-down")
       .forEach((element) =>
         element.classList.replace("fa-angle-down", "fa-angle-up")
       );
 
+    // this conditional manages visibility and fa-angle only for the current instance of @components/order/OrderHeaders.js (the one that has been clicked)
     if (
+      // if order is not rendered
       document.getElementById(order._id).classList.contains("not_render_orders")
     ) {
+      // render order
       document.getElementById(order._id).classList.remove("not_render_orders");
-      setAngleUp(false);
+
+      // set fa-angle to down
+      angleElement.current.classList.replace("fa-angle-up", "fa-angle-down");
+
+      // if order is rendered
     } else {
+      // stop render order
       document.getElementById(order._id).classList.add("not_render_orders");
-      setAngleUp(true);
+
+      // set fa-angle to up
+      angleElement.current.classList.replace("fa-angle-down", "fa-angle-up");
     }
   };
 
@@ -51,12 +66,7 @@ const OrderHeaders = ({ order }) => {
           <i className="fas fa-check"></i>
         </div>
         <div>
-          &nbsp;{" "}
-          {angleUp ? (
-            <i className="fas fa-angle-up"></i>
-          ) : (
-            <i className="fas fa-angle-down"></i>
-          )}
+          <i ref={angleElement} className="fas fa-angle-up"></i>
         </div>
       </div>
       <div id={order._id} className="orders">
