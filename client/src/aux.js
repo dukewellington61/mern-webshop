15bd663f5ea6e3e0845b1e4c4b321af27caff5aa
akoeth-portfolio <dukewellington61@googlemail.com>
2020-11-04 20:24:00 +0100

    hide scrollbar in iframe solved

--- client/src/aux.js
+++ client/src/aux.js
@@ -1,6 +1,12 @@
+// this code hides the scrollbars if this app is displayed in the iframe of the portfolio page
+// the only reason for this is that it simply looks better in the iframe without the scroll bars
+// the portfolio page sends a message containing the string "iframe" to the app.
+// the app picks the message up (using the code below) and then adds a class so some css can be applied
+// if the app is displayed elsewhere the code below won't fire
+
 window.addEventListener("message", (message) => {
   if (message.data === "iframe") {
     console.log("iframe");
-    document.querySelector("html").style.overflow = "hidden";
+    document.querySelector("html").classList.add("hideScrollBar");
   }
 });
